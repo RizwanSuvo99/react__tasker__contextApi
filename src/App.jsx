7db611cf1d9ee3baed85c99921e3cd780feef821
RizwanSuvo99 <rizwanshuvo@gmail.com>
2024-01-26 17:03:28 +0600

    Initial Boiler Plate Created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl underline">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, vitae?
+      </h1>
+    </>
+  );
+}
+
+export default App;
